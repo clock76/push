2dc6b9bb02f07fc66fd44415ee7f47588945166f
clock <timergalievtimur76@mail.ru>
2021-03-14 10:14:12 +0300

    push

---
+++ main.py
@@ -0,0 +1,6 @@
+from module import *
+print(my_print('slam dunk'))
+print(words_multiple('baby nigga ', 3))
+print(function('azazazazazazazaz'))
+print(function_2())
+print(function_3())
---
+++ module.py
@@ -0,0 +1,18 @@
+def my_print(haha):
+    return 'Simon said: '+ haha
+
+def words_multiple(words, amount):
+    return words * amount
+
+def function(words2):
+    return words2 + ' длинной ' + str(len(words2)) + ' байт'
+
+def function_2():
+    test = input ('введите: ')
+    return test
+
+def function_3():
+    test2 = input ('введите комманду: ')
+    if test2 == 'gamemode 0':
+        return 'выживание'
+    return test2
